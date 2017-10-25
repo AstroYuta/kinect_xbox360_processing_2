be5d6a84d72199fcee5844546562779e9d7ffcc6
Yuta Shimizu <yuta@Yutas-MacBook-Air.local>
2017-10-25 21:25:15 +0900

    ちょっと修正

--- build/source/kinect_2.java
+++ build/source/kinect_2.java
@@ -1,8 +1,27 @@
-import processing.opengl.*;
-import SimpleOpenNI.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.opengl.*; 
+import SimpleOpenNI.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class kinect_2 extends PApplet {
+
+
+
 SimpleOpenNI kinect;
 
-void setup() {
+public void setup() {
   size(1028, 768, OPENGL);
 
   kinect = new SimpleOpenNI(this);
@@ -13,13 +32,13 @@ void setup() {
   fill(255, 0, 0);
 }
 
-void draw() {
+public void draw() {
   kinect.update();
   background(255);
 
   // make User list
   IntVector userList = new IntVector();
-  // 検出されたユーザーのリストをVectorへ書き込む
+  // \u691c\u51fa\u3055\u308c\u305f\u30e6\u30fc\u30b6\u30fc\u306e\u30ea\u30b9\u30c8\u3092Vector\u3078\u66f8\u304d\u8fbc\u3080
   kinect.getUsers(userList);
 
   if (userList.size() > 0) {
@@ -44,7 +63,7 @@ void draw() {
   }
 }
 
-void drawSkeleton(int userId) {
+public void drawSkeleton(int userId) {
   kinect.drawLimb(userId, SimpleOpenNI.SKEL_HEAD, SimpleOpenNI.SKEL_NECK);
   kinect.drawLimb(userId, SimpleOpenNI.SKEL_NECK, SimpleOpenNI.SKEL_LEFT_SHOULDER);
   kinect.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_SHOULDER, SimpleOpenNI.SKEL_LEFT_ELBOW);
@@ -63,7 +82,7 @@ void drawSkeleton(int userId) {
   kinect.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_HIP, SimpleOpenNI.SKEL_LEFT_HIP);
 }
 
-void drawLimb(int userId, int jointType1, int jointType2) {
+public void drawLimb(int userId, int jointType1, int jointType2) {
     PVector jointPos1 = new PVector();
     PVector jointPos2 = new PVector();
     float confidence;
@@ -75,7 +94,7 @@ void drawLimb(int userId, int jointType1, int jointType2) {
     }
 }
 
-void onNewUser(SimpleOpenNI curContext, int userId)
+public void onNewUser(SimpleOpenNI curContext, int userId)
 {
   println("onNewUser - userId: " + userId);
   println("\tstart tracking skeleton");
@@ -83,12 +102,21 @@ void onNewUser(SimpleOpenNI curContext, int userId)
   curContext.startTrackingSkeleton(userId);
 }
 
-void onLostUser(SimpleOpenNI curContext, int userId)
+public void onLostUser(SimpleOpenNI curContext, int userId)
 {
   println("onLostUser - userId: " + userId);
 }
 
-void onVisibleUser(SimpleOpenNI curContext, int userId)
+public void onVisibleUser(SimpleOpenNI curContext, int userId)
 {
   //println("onVisibleUser - userId: " + userId);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "kinect_2" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
